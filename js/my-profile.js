53fff6ba3007e5cf7cdc9bd2a0ed781293772087
Matías López <69003973+matiaslopezz@users.noreply.github.com>
2020-11-17 15:41:11 -0300

    Add files via upload

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,53 @@
+function showInformation(){
+    personal_information = JSON.parse(localStorage.getItem("personal_information"));
+    if (personal_information){
+        if (document.getElementById("saved-name") != null){
+            document.getElementById("saved-name").innerHTML = personal_information.name;
+            document.getElementById("saved-surname").innerHTML = personal_information.surname;
+            document.getElementById("saved-age").innerHTML = personal_information.age;
+            document.getElementById("saved-email").innerHTML = personal_information.email;
+            document.getElementById("saved-phone").innerHTML = personal_information.phoneNumber;
+        }
+    }
+
+    profile_image = localStorage.getItem("profile_image");
+    if (profile_image){
+        document.getElementById("profile-image").innerHTML = `
+            <img src="`+ localStorage.getItem("profile_image") +`" alt="" style="width: 15rem";>
+        `;     
+    }
+}
+
+function saveInformation(){
+    document.getElementById("profile-form").addEventListener("submit", (e)=>{
+        var nameInput = document.getElementById("name").value
+        var surnameInput= document.getElementById("surname").value
+        var ageInput = document.getElementById("age").value
+        var emailInput = document.getElementById("email").value
+        var phoneInput = document.getElementById("phone-number").value
+    
+        var newObject = {
+            'name' : nameInput,
+            'surname' : surnameInput,
+            'age' : ageInput,
+            'email' : emailInput,
+            'phoneNumber' : phoneInput,
+        }
+
+        let imageURL = "https://i.ibb.co/qMjcYzG/profile-image.jpg"
+        localStorage.setItem("profile_image", imageURL);
+        
+        localStorage.setItem("personal_information",JSON.stringify(newObject))
+
+        showInformation();
+    })
+}
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    saveInformation();
+    showInformation();
 });
--- js/products.js
+++ js/products.js
@@ -28,24 +28,40 @@ function showProductsList(array){
         let product = array[i];
         
         //Elementos a incorporar al HTML
-        htmlContentToAppend += `
-        <a href="product-info.html?producto=` + product.name + `" class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + product.imgSrc + `" alt="` + `" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ product.name +`</h4>
-                        <small class="text-muted">` + 'Artículos vendidos:' + " " + product.soldCount + ` </small>
-                    </div>
-                    <p class="mb-1">` + product.description + `</p>
-                    <p class="mb-1">` + product.currency + " " + product.cost + `</p>
+        htmlContentToAppend += 
+        // `
+        // <a href="product-info.html?producto=` + product.name + `" class="list-group-item list-group-item-action">
+        //     <div class="row">
+        //         <div class="col-3">
+        //             <img src="` + product.imgSrc + `" alt="` + `" class="img-thumbnail">
+        //         </div>
+        //         <div class="col">
+        //             <div class="d-flex w-100 justify-content-between">
+        //                 <h4 class="mb-1">`+ product.name +`</h4>
+        //                 <small class="text-muted">` + 'Artículos vendidos:' + " " + product.soldCount + ` </small>
+        //             </div>
+        //             <p class="mb-1">` + product.description + `</p>
+        //             <p class="mb-1">` + product.currency + " " + product.cost + `</p>
+        //         </div>
+        //     </div>
+        // </div>
+        // </a>
+        // `;
+
+        `
+        <div class="col-md-4" style="margin-top: 25px">
+            <div class="card">
+                <img src="` + product.imgSrc + `" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">` + product.name + `</h5>
+                    <p class="card-text">` + product.description + `</p>
+                    <p class="card-text">` + product.currency + " " + product.cost + `</p>
+                    <p class="card-text text-muted"> Artículos vendidos:` + " " + product.soldCount + `</p>
+                    <a href="product-info.html?producto=` + product.name + `" class="btn btn-primary">Ver más detalles</a>
                 </div>
             </div>
         </div>
-        </a>
-        `; 
+        `;
     }
     //Incorporamos la información al HTML
     document.getElementById('prod-list-container').innerHTML = htmlContentToAppend;
